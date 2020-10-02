c9aa5938314b5cff65a5754e9415f42f8bcc542b
Joelsoliz52 <65687705+Joelsoliz52@users.noreply.github.com>
2020-10-02 19:45:12 -0400

    Add files via upload

--- quizc/console/quiz_ui_handler.py
+++ quizc/console/quiz_ui_handler.py
@@ -2,9 +2,20 @@ from quizc.console.quiz_input_handler import QuestionInputHandler
 from quizc.console.quiz_ui_menu import QuizUIMenu
 from quizc.model.quiz import Quiz
 from quizc.model.quiz_answers import QuizAnswer, Answer
+from json import JSONEncoder
+
+class MyClass:
+    def __init__(self, value):
+        self.value = value
+
+
+class MyEncoder(JSONEncoder):
+    def default(self, o):
+        return o.__dict__
 
 
 class QuizUIHandler(object):
+
 
     @staticmethod
     def create_quiz() -> Quiz:
@@ -18,10 +29,16 @@ class QuizUIHandler(object):
         print("Quiz:" + quiz.title)
         for question in quiz.questions:
             answers = question_handler.ask_question_value(question)
-            answer = Answer(answers, question)
+            answer = Answer(question, answers)
             quiz_answer.add_answer(answer)
 
         return quiz_answer
+
+        myclass = MyClass(quiz_answer)
+        json = MyEncoder().encode(myclass)
+        f = open("myform.json", "w")
+        f.write(json)
+        f.close()
 
     @staticmethod
     def show_quiz(quiz_answer):
@@ -29,5 +46,6 @@ class QuizUIHandler(object):
         print("=============================================")
         for answer in quiz_answer.answers:
             print(answer)
+
 
         return quiz_answer
